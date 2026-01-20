c0a96a84b59d8e9041f4f5e678f52a177672cf09
Omer Oreg <omeroreg@gmail.com>
2026-01-20 14:11:10 +0200

    Remove old files

--- old-matlab/@Skycam/private/takeExposure.m
+++
@@ -1,6 +0,0 @@
-% Take exposure with astronomical cameras, called by the timer
-function takeExposure(F)
-    if ~isempty(F.FileCheck) && F.CameraType == "ASTRO"
-        F.CameraRes.takeExposure(F.ExpTime);
-    end
-end
